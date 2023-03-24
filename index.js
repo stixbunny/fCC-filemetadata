b891bef001a49b088a7904281b4d9a580b04917a
stixbunny <milenko.castillo@gmail.com>
2023-03-24 17:42:49 -0300

    Update index.js - post /api/fileanalyze

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer');
+const upload = multer().single("upfile");
+require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +13,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+app.post("/api/fileanalyse", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) return console.error(err);
+    const result = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    }
+    res.json(result);
+  })
 });
